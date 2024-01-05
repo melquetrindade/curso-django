7f61d9e10d369cde29a9ab3d3c8f422f34a430d4
melquetrindade <melquetrindade654@gmail.com>
2024-01-05 15:09:32 -0300

    Finalizando as aulas 11 ate o 12

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from . models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     """ Página principal do Learning_log """
@@ -17,3 +20,16 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-data_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ Adiciona um novo assunto """
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
