25f9da563d22295b71499770a7e1c6e2096a252f
melquetrindade <melquetrindade654@gmail.com>
2024-01-05 11:18:58 -0300

    Finalizando as aulas 7 ate o 10

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from . models import Topic
 
 def index(request):
     """ Página principal do Learning_log """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Mostra todos os assuntos """
+    topics = Topic.objects.order_by('data_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Mostra todos os assuntos e suas entradas """
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-data_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
